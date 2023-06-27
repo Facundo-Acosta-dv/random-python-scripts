811775ea6d05b522053487abba96ac9788366683
Facundo Acosta <facundo.acosta.dv@gmail.com>
2023-06-27 07:44:35 -0300

    rm random file

--- python/euge.py
+++
@@ -1,6 +0,0 @@
-tortitas = []
-cantidad = int(input("cantidad de ingredientes?: "))
-for i in range(cantidad):
-	ingrediente = input("nombre del ingrediente: ")
-	tortitas.append(ingrediente)
-print(tortitas)
